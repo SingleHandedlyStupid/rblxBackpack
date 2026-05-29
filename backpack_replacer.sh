13fd38b88443d551407c7f5ba6a8b74687d37c7b
SingleHandedlyStupid <shitjohndoesdumb@gmail.com>
2026-05-28 23:18:58 -0400

    Update backpack_replacer.sh

--- backpack_replacer.sh
+++ backpack_replacer.sh
@@ -5,7 +5,7 @@
 UI_DIR="/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse"
 FIND_DIR="/Documents"
 
-rm $UI_DIR/ArrowFarCursor.pn
+rm $UI_DIR/ArrowFarCursor.png
 
 sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowFarCursor.png"
     sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowCursor.png"
