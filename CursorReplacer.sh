1b95a7c7912b6bffa184a5f2fca0b97f2e07f5b8
SingleHandedlyStupid <shitjohndoesdumb@gmail.com>
2026-05-29 14:11:24 -0400

    Update CursorReplacer.sh

--- CursorReplacer.sh
+++ CursorReplacer.sh
@@ -2,13 +2,26 @@
 
 # The correct path to Roblox textures
 UI_DIR="/Applications/Roblox.app/Contents/Resources/content/textures/Cursors/KeyboardMouse"
+BACKUP_DIR="/Downloads"
 
 # The correct path to YOUR Documents folder
 FIND_DIR="$HOME/Downloads"
 
-# Running the copy commands
-sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowFarCursor.png"
-sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowCursor.png"
+if [ ! -d "$BACKUP_DIR/backupCursor" ]; then
+    echo "Backup doesnt exist."
+    echo "Making now!"
+    mkdir "$BACKUP_DIR/backupCursor"
+    # rm -rf "$BACKUP_DIR/backupCursor"
+fi
 
-echo "--- Finished! Restarting Roblox to see changes. ---"
-killall "RobloxPlayer"
+read -p "Enter your name: " continuerun
+
+if [ ! -v continuerun ]; then
+  echo "well, crap"
+  else
+    # Running the copy commands
+    sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowFarCursor.png"
+    sudo cp "$FIND_DIR/cursor.png" "$UI_DIR/ArrowCursor.png"
+    
+    echo "--- Finished! Restart Roblox to see changes. ---"
+fi
